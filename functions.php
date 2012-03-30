4d54e9da2cb405bedbe423fd63ff40204da84ecb
Christopher Davis <cdavis9999@gmail.com>
2012-03-30 14:54:03 -0500

    Adding Height & Width to Login Image
    users can specify the height/width of the login log now

--- functions.php
+++ functions.php
@@ -5,8 +5,10 @@ require_once( trailingslashit( get_stylesheet_directory() ) . 'branding.php' );
 // Create a new instance of the Custom_Admin_Branding class and pass in your args
 new Custom_Admin_Branding( array( 
     'login_url'       => 'http://example.com', 
-    'login_image'     => 'http://placebear.com/273/63',
+    'login_image'     => 'http://placebear.com/400/100',
     'login_title'     => 'This is a title',
+    'login_height'    => '100px',
+    'login_width'     => '400px',
     'designer_url'    => 'http://christopherdavis.me',
     'designer_anchor' => 'Christopher Davis',
     'favicon_url'     => 'http://placebear.com/16/16',
